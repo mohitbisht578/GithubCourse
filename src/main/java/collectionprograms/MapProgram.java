744a3df3141ad5b93cc219766c4b7e4285630372
Mohit Bisht <mohitbisht@Mohits-MacBook-Air.local>
2021-12-14 18:26:05 +0530

    updated map program

--- src/main/java/collectionprograms/MapProgram.java
+++ src/main/java/collectionprograms/MapProgram.java
@@ -1,18 +1,24 @@
-import java.util.*;  
+package collectionprograms;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class MapProgram {  
 public static void main(String[] args) {  
-    Map map=new HashMap();  
+	
+	Map<Integer, String> map = new HashMap<>();  
+	
     //Adding elements to map  
     map.put(1,"Amit");  
     map.put(5,"Rahul");  
     map.put(2,"Jai");  
     map.put(6,"Amit");  
+    
+    
     //Traversing Map  
-    Set set=map.entrySet();//Converting to Set so that we can traverse  
-    Iterator itr=set.iterator();  
-    while(itr.hasNext()){  
-        //Converting to Map.Entry so that we can get key and value separately  
-        Map.Entry entry=(Map.Entry)itr.next();  
-        System.out.println(entry.getKey()+" "+entry.getValue());  
-    }  
+    for(Map.Entry<Integer, String> entry : map.entrySet()) {
+    	System.out.println(entry.getKey() + " : " + entry.getValue());
+    }
+  }
+    
 }  
